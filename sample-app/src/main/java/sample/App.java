cda3f893a42dabf5e5ab5a4f41eadff7fa01c360
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2022-10-22 01:25:01 +0000

    Release 0.0.53-2-ga837a73

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -1,9 +1,9 @@
-// import com.fern.nursery.api.client.FernNurseryApiClient
 package sample;
 
 import java.lang.String;
 
 public final class App {
   public static void main(String[] args) {
+    // import com.fern.nursery.api.client.FernNurseryApiClient
   }
 }
